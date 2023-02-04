0df68cb0f018e4f133e25fc60edaf67a179fe6af
hieuck <30469654+hieuck@users.noreply.github.com>
2023-02-04 16:27:22 +0700

    Update Direct-Link.py
    Cập nhật phiên bản

--- Direct-Link.py
+++ Direct-Link.py
@@ -2,39 +2,58 @@ import re
 
 def main():
     links = []
-    input_source = input("Enter 'f' to read from a file or press enter to type the link: ")
-    if input_source == "f":
-        with open("input.txt", "r") as f:
-            links = f.readlines()
-        links = [x.strip() for x in links]
-    else:
+    input_source = input("Enter 'f' to read from a file or 't' to type the link (or press enter to quit): ")
+    while input_source not in ["f", "F", "t", "T", ""]:
+        print("Invalid input. Please try again.")
+        input_source = input("Enter 'f' to read from a file or 't' to type the link (or press enter to quit): ")
+    if input_source in ["f", "F"]:
+        try:
+            with open("input.txt", "r") as f:
+                links = f.readlines()
+            links = [x.strip() for x in links]
+        except:
+            print("An error occurred while opening the file. Exiting.")
+            exit()
+    elif input_source in ["t", "T"]:
         while True:
             link = input("Enter the link (or press enter to quit): ")
             if link == "":
                 break
             links.append(link)
+    else:
+        return
 
     new_links = []
     for link in links:
+        new_link_o = new_link_d = new_link_g = None
         if "onedrive.live.com" in link:
-            new_link_o = ""
             if re.search(r"https://.*width=", link):
                 new_link_o = re.sub(r"embed", "download", re.search(r"https://.*width=", link).group())
                 new_link_o = new_link_o.rstrip("\" width=")
-            new_links.append(new_link_o)
         elif "www.dropbox.com" in link:
             new_link_d = link.replace("www.dropbox.com", "dl.dropboxusercontent.com")
-            new_links.append(new_link_d)
         elif "drive.google.com" in link:
             google_drive_link = re.search(r"https://drive.google.com/file/d/(.*)/view", link)
             if google_drive_link:
                 new_link_g = "https://drive.google.com/uc?export=download&id=" + google_drive_link.group(1)
-                new_links.append(new_link_g)
 
-    with open("output.txt", "w") as f:
-        for l in new_links:
-            f.write(l + "\n")
-    print("Done!")
+        if new_link_o or new_link_d or new_link_g:
+            new_links.append(new_link_o or new_link_d or new_link_g)
+        else:
+            print(f"{link} is not a link from OneDrive, Dropbox, or Google Drive.")
+
+    if new_links:
+        print("\n".join(new_links))
+        input("Press enter to write to output.txt and quit.")
+        try:
+            with open("output.txt", "w") as f:
+                for l in new_links:
+                    f.write(l + "\n")
+        except:
+            print("An error occurred while opening the file. Exiting.")
+            exit()
+    else:
+        print("No links found.")
 
 if __name__ == "__main__":
     main()
